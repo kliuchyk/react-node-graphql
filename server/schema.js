67a3fc2d546b698bd44242a2786fe24768278474
kliuchyk <kliuchyk@gmail.com>
2021-02-05 11:01:59 +0200

    add basic setup for server with graphql schema

---
+++ server/schema.js
@@ -0,0 +1,38 @@
+const { buildSchema } = require("graphql");
+
+const schema = buildSchema(`
+
+  type User {
+    id: ID,
+    userName: String,
+    age: Int,
+    posts: [Post]
+  }
+
+  type Post {
+    id: ID,
+    title: String,
+    content: String 
+  }
+
+  input UserInput {
+    id: ID,
+    userName: String!,
+    age: Int!,
+    posts: [PostInput]
+  }
+
+  input PostInput {
+    id: ID,
+    title: String!,
+    content: String!,  
+  }
+
+  type Query {
+    getAllUsers: [User]
+    getUser(id: ID): User
+  }
+
+`);
+
+module.exports = schema;
